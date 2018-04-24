bff10d1023a01962fb591c1715148d7d7cdf1411
Wagner Leonardi <leonardiwagner@gmail.com>
2018-04-24 16:17:41 -0300

    transpose and zip list functions

--- src/List.test.ts
+++ src/List.test.ts
@@ -41,7 +41,7 @@ describe('List', () => {
 
   it('should fold a list', () => {
     const list = new List<number>([1, 2, 3, 4, 5] as number[]);
-    const result = list.fold((a, b) => a + b, "U");
+    const result = list.fold<string>((a, b) => a + b, "U");
     const expectedResult = "U12345"
 
     expect(result).to.deep.equal(expectedResult);
@@ -51,6 +51,67 @@ describe('List', () => {
     const list = new List<number>([1] as number[]);
     const result = list.fold((a, b) => a + b, "U");
     const expectedResult = "U1"
+
+    expect(result).to.deep.equal(expectedResult);
+  })
+
+  it('should concat a list', () => {
+    const list = new List<number>([1, 2] as number[]);
+    const list2 = new List<number>([3, 4] as number[]);
+
+    const result = list.concat(list2);
+    const expectedResult = new List<number>([1, 2, 3, 4] as number[]);
+
+    expect(result).to.deep.equal(expectedResult);
+  })
+
+  it('should check empty list', () => {
+    const list = new List<number>([] as number[]);
+
+    const result = list.isEmpty;
+    const expectedResult = true;
+
+    expect(result).to.deep.equal(expectedResult);
+  })
+
+  it('should check not empty list', () => {
+    const list = new List<number>([1, 2] as number[]);
+
+    const result = list.isEmpty;
+    const expectedResult = false;
+
+    expect(result).to.deep.equal(expectedResult);
+  })
+
+  it('should reverse list', () => {
+    const list = new List<number>([1, 2, 3, 4] as number[]);
+
+    const result = list.reverse();
+    const expectedResult = new List<number>([4, 3, 2, 1] as number[]);
+
+    expect(result).to.deep.equal(expectedResult);
+  })
+
+  it('should transpose list', () => {
+    const list = new List<number>([10, 11] as number[]);
+
+    const list2 = new List<number>([20] as number[]);
+    const list3 = new List<number>([] as number[]);
+    const list4 = new List<number>([30, 31, 32] as number[]);
+
+    const result = list.transpose(new List([list2, list3, list4]));
+
+    const expectedResult = new List([new List([10,20,30]), new List([11,31]), new List([32])]);
+
+    expect(result).to.deep.equal(expectedResult);
+  })
+
+  it('should zip list', () => {
+    const list = new List([10, 11]);
+
+    const result = list.zip(new List([1, 2, 3]));
+
+    const expectedResult = new List([new List([10,1]), new List([11,2]), new List([undefined ,3])]);
 
     expect(result).to.deep.equal(expectedResult);
   })
--- src/List.ts
+++ src/List.ts
@@ -1,14 +1,33 @@
 export class List<T> {
-  private elements: Array<T>;
+  public elements: Array<T>;
   public length: number;
   public head: T;
+  public init: Array<T>;
   public tail: Array<T>;
+  public last: T;
+  public isEmpty: boolean;
 
   constructor(elements: Array<T>) {
     this.elements = elements;
     this.length = elements.length;
     this.head = elements.slice(0, 1)[0];
+    this.init = elements.slice(0, elements.length - 1);
     this.tail = elements.slice(1);
+    this.last = elements.slice(elements.length - 1, elements.length)[0];
+    this.isEmpty = elements.length === 0;
+  }
+
+  add(element: T): List<T> {
+    return this.concat(new List([element]));
+  }
+
+  get(index: number): T {
+    return this.elements[index];
+  }
+
+  concat(list: List<T>): List<T> {
+    const concatElements = this.elements.concat(list.elements);
+    return new List<T>(concatElements);
   }
 
   forEach(f: (element: T) => any): void {
@@ -18,7 +37,7 @@ export class List<T> {
     }
   }
 
-  map<U>(f: (element: T) => U): List<U> {
+  map<U = T>(f: (element: T) => U): List<U> {
     const resultElements = [] as U[];
 
     for(let i = 0; i < this.elements.length; i++) {
@@ -44,7 +63,7 @@ export class List<T> {
     return result;
   }
 
-  fold<U>(f: (accumulator: U, element: T) => U, initialValue: U): U {
+  fold<U = T>(f: (accumulator: U, element: T) => U, initialValue: U): U {
     let result: U  = initialValue;
 
     for(let i = 0; i < this.elements.length; i++) {
@@ -53,5 +72,43 @@ export class List<T> {
     }
 
     return result;
+  }
+
+  reverse(): List<T> {
+    const newList = [];
+    for(let i = this.elements.length - 1; i >= 0; i--) {
+      const element = this.elements[i];
+      newList.push(element);
+    }
+
+    return new List(newList);
+  }
+
+  transpose(listOfList: List<List<T>>): List<List<T>> {
+    const arrayOfLists = this.elements.map(x =>  new List([x]))
+
+    for(let i = 0; i < listOfList.length; i++) {
+      for(let j = 0; j < listOfList.get(i).length; j++) {
+        if(j > arrayOfLists.length - 1) {
+          arrayOfLists.push(new List([]))
+        }
+
+        const element = listOfList.get(i).get(j)
+        arrayOfLists[j] = arrayOfLists[j].add(element)
+      }
+    }
+
+    return new List(arrayOfLists);
+  }
+
+  zip(list: List<T>): List<List<T>> {
+    const sizeOfList = (list.length > this.length) ? list.length : this.length;
+
+    const elements = [];
+    for(let i = 0; i < sizeOfList; i++) {
+      elements.push(new List([this.elements[i], list.get(i)]))
+    }
+
+    return new List(elements)
   }
 }
